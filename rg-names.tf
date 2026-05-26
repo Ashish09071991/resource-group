562441943d39537367e18bdfa6759ebe29c94599
Ashish09071991 <ashish38351@gmail.com>
2026-05-27 00:00:56 +0530

    Update rg-names.tf
    1 RG added

--- rg-names.tf
+++ rg-names.tf
@@ -1,3 +1,9 @@
 resource "azurerm_resource_group" "rgnames" {
   name = "rg-ashish"
   location = "eastus"
+}
+resource "azurerm_resource_group" "rgnames1" {
+  name = "rg-ashish1"
+  location = "westus"
+}
+
